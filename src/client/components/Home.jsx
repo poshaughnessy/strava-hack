d90a0bbe049eddc85e3b9dfc6a82f4c616f1cd47
Peter O'Shaughnessy <peter.oshaughnessy@pebblecode.com>
2016-06-03 17:14:32 +0100

    Starting on D3 graph

--- src/client/components/Home.jsx
+++ src/client/components/Home.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import graph from '../graph';
 
-const HomePage = () => {
-  return <p>Hello</p>;
+class HomePage extends React.Component {
+
+  componentDidMount() {
+    graph(this.refs.graphContainer);
+  }
+
+  render() {
+    return (
+      <div>
+        <header>FitStat</header>
+        <div className="main" ref="graphContainer">
+        </div>
+      </div>
+    );
+  }
+
 };
 
 export default HomePage;
