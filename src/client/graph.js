132245de49dee0af6530e641f2bacf181e2e2529
Peter O'Shaughnessy <peter.oshaughnessy@pebblecode.com>
2016-06-03 17:40:00 +0100

    Working on distance graph

--- src/client/graph.js
+++ src/client/graph.js
@@ -1,20 +1,22 @@
 import d3 from 'd3';
 
+const timeFormat = d3.time.format('%Y-%m-%dT%H:%M:%SZ');
+
+function parseDate(date) {
+  return timeFormat.parse(date);
+}
+
 export default function(element) {
 
-  var margin = {top: 20, right: 80, bottom: 30, left: 50},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
-
-  var parseDate = d3.time.format('%Y%m%d').parse;
+  var margin = {top: 20, right: 20, bottom: 80, left: 50},
+    width = window.innerWidth - margin.left - margin.right,
+    height = window.innerHeight - margin.top - margin.bottom;
 
   var x = d3.time.scale()
     .range([0, width]);
 
   var y = d3.scale.linear()
     .range([height, 0]);
-
-  var color = d3.scale.category10();
 
   var xAxis = d3.svg.axis()
     .scale(x)
@@ -25,8 +27,8 @@ export default function(element) {
     .orient('left');
 
   var line = d3.svg.line()
-    .x(function(d) { return x(d.date); })
-    .y(function(d) { return y(d.temperature); });
+    .x(function(d) { return x(d.start_date); })
+    .y(function(d) { return y(d.distance); });
 
   var svg = d3.select(element).append('svg')
     .attr('width', width + margin.left + margin.right)
@@ -34,40 +36,16 @@ export default function(element) {
     .append('g')
     .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-  d3.tsv('data.tsv', function(error, data) {
-    color.domain(d3.keys(data[0]).filter(function (key) {
-      return key !== 'date';
-    }));
+  d3.json('activities.json', function(error, data) {
+
+    if (error) throw error;
 
     data.forEach(function (d) {
-      d.date = parseDate(d.date);
+      d.start_date = parseDate(d.start_date);
     });
 
-    var cities = color.domain().map(function (name) {
-      return {
-        name: name,
-        values: data.map(function (d) {
-          return {date: d.date, temperature: +d[name]};
-        })
-      };
-    });
-
-    x.domain(d3.extent(data, function (d) {
-      return d.date;
-    }));
-
-    y.domain([
-      d3.min(cities, function (c) {
-        return d3.min(c.values, function (v) {
-          return v.temperature;
-        });
-      }),
-      d3.max(cities, function (c) {
-        return d3.max(c.values, function (v) {
-          return v.temperature;
-        });
-      })
-    ]);
+    x.domain(d3.extent(data, function(d) { return d.start_date; }));
+    y.domain(d3.extent(data, function(d) { return d.distance; }));
 
     svg.append('g')
       .attr('class', 'x axis')
@@ -81,51 +59,12 @@ export default function(element) {
       .attr('transform', 'rotate(-90)')
       .attr('y', 6)
       .attr('dy', '.71em')
-      .style('text-anchor', 'end')
-      .text('Temperature (ºF)');
+      .style('text-anchor', 'end');
 
-    var city = svg.selectAll('.city')
-      .data(cities)
-      .enter().append('g')
-      .attr('class', 'city');
-
-    city.append('path')
+    svg.append('path')
+      .datum(data)
       .attr('class', 'line')
-      .attr('d', function (d) {
-        return line(d.values);
-      })
-      .style('stroke', function (d) {
-        return color(d.name);
-      });
-
-    city.append('g').selectAll('circle')
-      .data(function (d) {
-        return d.values
-      })
-      .enter()
-      .append('circle')
-      .attr('r', 2)
-      .attr('cx', function (dd) {
-        return x(dd.date)
-      })
-      .attr('cy', function (dd) {
-        return y(dd.temperature)
-      })
-      .attr('fill', 'none')
-      .attr('stroke', 'black')
-
-    city.append('text')
-      .datum(function (d) {
-        return {name: d.name, value: d.values[d.values.length - 1]};
-      })
-      .attr('transform', function (d) {
-        return 'translate(' + x(d.value.date) + ',' + y(d.value.temperature) + ')';
-      })
-      .attr('x', 3)
-      .attr('dy', '.35em')
-      .text(function (d) {
-        return d.name;
-      });
+      .attr('d', line);
 
   });
 
